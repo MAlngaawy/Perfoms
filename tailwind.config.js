7fd3918ee668da24cd54d00abc74405d6f161da6
MAlngaawy <m.alngaawy@gmail.com>
2022-10-30 05:44:03 +0200

    add prittier file & some tailwind theme channges

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ const path = require("path");
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        perfBlue: "#2F80ED",
+        perfGray: "#8D8D8D",
+        perfGray1: "#333333",
+        perfGray2: "#4f4f4f",
+        perfGray3: "#828282",
+        perfOfWhite: "#f9fbfc00",
+        perfSecondary: "#C32B43",
+      },
+      screens: {
+        lg: "1200px",
+      },
+    },
   },
   plugins: [
     require(path.resolve(__dirname, "src/@main/tailwind/plugins/icon-size")),
