2b9adddda5114ea84cec12f67d7cc79b87e86128
MAlngaawy <m.alngaawy@gmail.com>
2022-11-01 23:04:21 +0200

    add the toggile button to the toolbar

--- src/app/layouts/layout/components/Toolbar.tsx
+++ src/app/layouts/layout/components/Toolbar.tsx
@@ -1,14 +1,24 @@
-import { Button } from "@mantine/core";
+import { Button, Grid } from "@mantine/core";
 import { userApi } from "app/store/user/userApi";
 import Cookies from "js-cookie";
 import { useDispatch } from "react-redux";
+import AppIcons from "@main/core/AppIcons";
 
-type Props = {};
+type Props = {
+  opened: boolean;
+  setOpened: any;
+};
 
-const Toolbar = (props: Props) => {
+const Toolbar = ({ setOpened }: Props) => {
   const dispatch = useDispatch();
   return (
-    <div>
+    <div className="bg-red w-full flex justify-between items-center">
+      <button
+        className="block lg:hidden text-black border-0"
+        onClick={() => setOpened(true)}
+      >
+        <AppIcons className="w-6 h-6 " icon="Bars3BottomLeftIcon:solid" />
+      </button>
       <Button
         onClick={() => {
           Cookies.remove("token");
