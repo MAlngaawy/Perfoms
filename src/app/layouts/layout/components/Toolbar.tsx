2c575228ce486c0dfd70cbe87807f1404862efb0
MAlngaawy <m.alngaawy@gmail.com>
2022-11-07 16:57:01 +0200

    create the messages and notifications dropdown menu

--- src/app/layouts/layout/components/Toolbar.tsx
+++ src/app/layouts/layout/components/Toolbar.tsx
@@ -1,4 +1,4 @@
-import { Button, Grid } from "@mantine/core";
+import { Avatar, Button, Divider, Grid, Menu, Text } from "@mantine/core";
 import { userApi } from "~/app/store/user/userApi";
 import Cookies from "js-cookie";
 import { useDispatch } from "react-redux";
@@ -17,29 +17,69 @@ const Toolbar = ({ setOpened }: Props) => {
   let href = window.location.href;
   let routeName = href.slice(href.lastIndexOf("/") + 1, href.length);
 
-  if (routeName === "home") {
-    return <HomeFirstNav userName="Ahmed Kamal" />;
-  }
+  // if (routeName === "home") {
+  //   return <HomeFirstNav userName="Ahmed Kamal" />;
+  // }
 
   return (
-    // <FirstNav pageName={routeName} />
-    <div className="bg-fadedGray p-2 w-full flex justify-between items-center">
-      <button
-        className="block md:hidden text-black border-0"
-        onClick={() => setOpened(true)}
-      >
-        <AppIcons className="w-6 h-6 " icon="Bars3BottomLeftIcon:solid" />
-      </button>
-      {/* <Button
-        onClick={() => {
-          Cookies.remove("token");
-          dispatch(userApi.util.resetApiState());
-        }}
-        variant="default"
-      >
-        Logout
-      </Button> */}
-    </div>
+    <nav className="w-full flex justify-between items-center shadow-md p-4 bg-white">
+      <div className="bg-fadedGray p-2 w-full flex justify-between items-center">
+        <button
+          className="block md:hidden text-black border-0"
+          onClick={() => setOpened(true)}
+        >
+          <AppIcons className="w-6 h-6 " icon="Bars3BottomLeftIcon:solid" />
+        </button>
+        <div className="clubLogo hidden lg:block">Club Logo</div>
+      </div>
+      <div className="right flex gap-2 justify-center items-center">
+        {/* Notifications Menu */}
+        <Menu trigger="hover" shadow="md" width={200}>
+          <Menu.Target>
+            <Avatar className="cursor-pointer" radius="xl">
+              <AppIcons
+                className="w-5 h-5 text-perfGray3"
+                icon="BellIcon:outline"
+              />
+            </Avatar>
+          </Menu.Target>
+
+          <Menu.Dropdown>
+            <h2 className="m-2 text-perfLightBlack text-sm">Notifications</h2>
+            <Divider />
+            <Menu.Label>
+              <div>This is The first Nvigation</div>
+            </Menu.Label>
+            <Menu.Label>
+              <div>This is The Second one </div>
+            </Menu.Label>
+          </Menu.Dropdown>
+        </Menu>
+
+        {/* Messages Menu */}
+        <Menu trigger="hover" shadow="md" width={200}>
+          <Menu.Target>
+            <Avatar className="cursor-pointer" radius="xl">
+              <AppIcons
+                className="w-5 h-5 text-perfGray3"
+                icon="EnvelopeIcon:outline"
+              />
+            </Avatar>
+          </Menu.Target>
+
+          <Menu.Dropdown>
+            <h2 className="m-2 text-perfLightBlack text-sm">Messages</h2>
+            <Divider />
+            <Menu.Label>
+              <div>Message One</div>
+            </Menu.Label>
+            <Menu.Label>
+              <div> Message Two </div>
+            </Menu.Label>
+          </Menu.Dropdown>
+        </Menu>
+      </div>
+    </nav>
   );
 };
 
