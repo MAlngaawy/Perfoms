d014edb8a30ab98fe138980f7f5029bf79cc688f
MAlngaawy <m.alngaawy@gmail.com>
2022-11-10 16:13:11 +0200

    separate topbar component

--- src/app/pages/coachHome/CoachHome.tsx
+++ src/app/pages/coachHome/CoachHome.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Radio } from "@mantine/core";
+import TopBar from "./components/TopBar";
 
 type Props = {};
 
@@ -14,48 +15,10 @@ const CoachHome = (props: Props) => {
   return (
     <div className="coah-home">
       <div className="flex gap-4">
-        <Select checked={checked} value="Attendance" setChecked={setChecked} />
+        <TopBar checked={checked} setChecked={setChecked} />
       </div>
     </div>
   );
 };
 
 export default CoachHome;
-
-const Select = ({
-  value,
-  checked,
-  setChecked,
-}: {
-  value: "Attendance" | "Performance" | "Team info";
-  checked: "Attendance" | "Performance" | "Team info";
-  setChecked: any;
-}) => {
-  return (
-    <Radio.Group
-      value={checked}
-      onChange={(checkedValue) => setChecked(checkedValue)}
-      name="favoriteFramework"
-      spacing="xl"
-      offset="md"
-      size="sm"
-    >
-      <Radio
-        sx={{
-          ".mantine-Radio-body": {
-            backgroundColor: "#fff",
-            alignItems: "baseline",
-            cursor: "pointer",
-            padding: "10px 25px",
-            borderRadius: "50px",
-          },
-        }}
-        className=""
-        value="Attendance"
-        label="Attendance"
-      />
-      <Radio value="Performance" label="Performance" />
-      <Radio value="Team info" label="Team info" />
-    </Radio.Group>
-  );
-};
