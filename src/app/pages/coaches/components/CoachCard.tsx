ebbfaecf0bc5eb4a7ca5b93a3031b51fffb65cde
omarfarids <omar.farid007@gmail.com>
2022-11-12 21:41:24 +0200

    editing hover styling on coaches card

--- src/app/pages/coaches/components/CoachCard.tsx
+++ src/app/pages/coaches/components/CoachCard.tsx
@@ -23,28 +23,24 @@ const CoachCard = ({
 }: Props) => {
   const navigate = useNavigate();
   return (
-    <div className="coachCard h-full transition-all group hover:bg-perfBlue hover:text-white bg-white text-perfLightBlack rounded-3xl text-center p-5 font-medium">
+    <div className="coachCard h-full transition-all group bg-white text-perfLightBlack rounded-3xl text-center p-5 font-medium">
       <h3 className="text-base font-medium text-center">
         {role ? role : "Coach"}
       </h3>
       <div className="flex justify-center items-center">
         <img
-          className="my-4 w-32 h-32 object-cover transition-all delay-75 rounded-lg group-hover:border border-white box-border"
+          className="my-4 w-32 h-32 object-cover transition-all delay-75 rounded-lg border-white box-border"
           src={photo ? photo : "/assets/images/avatar.webp"}
           alt="Profile_Picture"
         />
       </div>
       <h2 className="text-xl">{name}</h2>
-      <h4 className="text-perfBlue group-hover:text-white text-xs">
-        {sport} Coach
-      </h4>
+      <h4 className="text-perfBlue text-xs">{sport} Coach</h4>
 
       <div className="flex justify-around gap-4 my-4 text-left">
         <div className="education flex flex-col">
           <h3 className="text-base">Education</h3>
-          <p className=" text-perfGray3 group-hover:text-white text-xs font-normal">
-            {education}
-          </p>
+          <p className=" text-perfGray3 text-xs font-normal">{education}</p>
         </div>
         <div className="flex justify-between items-center">
           <div className="teams flex flex-col">
@@ -52,7 +48,7 @@ const CoachCard = ({
             <div className="flex flex-col">
               {teams.map((item) => (
                 <h4
-                  className="text-perfGray3 group-hover:text-white text-xs font-normal"
+                  className="text-perfGray3 text-xs font-normal"
                   key={item.id}
                 >
                   {" "}
@@ -66,7 +62,7 @@ const CoachCard = ({
       {/* <Link to={`${location.pathname}/single-coach/${id}`}> */}
       <Button
         onClick={() => navigate(`${id}`)}
-        className="text-perfBlue border border-perfBlue  group-hover:text-white group-hover:border-white font-normal text-base py-2 px-4 my-4 rounded-xl"
+        className="text-perfBlue border border-perfBlue hover:text-white hover:border-white font-normal text-base py-2 px-4 my-4 rounded-xl"
       >
         View full profile
       </Button>
