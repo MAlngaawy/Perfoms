f7b210af43cc7c90f7149300df65128a01dc46ae
MAlngaawy <m.alngaawy@gmail.com>
2022-11-12 12:55:58 +0200

    finish the attendances table

--- src/app/pages/coachHome/components/AttendanceTable.tsx
+++ src/app/pages/coachHome/components/AttendanceTable.tsx
@@ -1,159 +1,194 @@
 import React from "react";
-import { Table, Checkbox } from "@mantine/core";
+import { Table, Checkbox, Avatar } from "@mantine/core";
 
 type Props = {};
 
-const elements = [
+const players = [
   {
-    check: "Check One ",
-    name: "Name One",
+    name: "mohammed Ali",
+    avatar:
+      "https://images.squarespace-cdn.com/content/v1/5c2efbc95417fc1b3d3040ad/1623735675824-LXS5FG198B0MLQCKQC0P/Modern+School+Photos.jpg",
+    id: 1,
   },
   {
-    check: "Check Two",
-    name: "Name Two",
+    name: "Ahmed Salah",
+    avatar:
+      "https://images.squarespace-cdn.com/content/v1/5c2efbc95417fc1b3d3040ad/1623728514460-NKFH3X6TIK8JXUESNRQD/Modern+School+Portraits+Perth.jpg",
+    id: 2,
   },
   {
-    check: "Check Three",
-    name: "Name Three",
+    name: "Kareem Momen",
+    avatar:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjh6VSzXG8EgTqDjm0JJ7CTlDVFnIHh6X9dPgBDqxt2kUOGgH7NQy0Ey6oPhEn5TVQbvI&usqp=CAU",
+    id: 3,
   },
   {
-    check: "Check One ",
-    name: "Name One",
+    name: "Khaled Momen",
+    avatar:
+      "https://www.anthropics.com/portraitpro/img/page-images/homepage/v22/what-can-it-do-5B.jpg",
+    id: 3,
   },
   {
-    check: "Check Two",
-    name: "Name Two",
+    name: "mohammed Ali",
+    avatar:
+      "https://images.squarespace-cdn.com/content/v1/5c2efbc95417fc1b3d3040ad/1623735675824-LXS5FG198B0MLQCKQC0P/Modern+School+Photos.jpg",
+    id: 1,
   },
   {
-    check: "Check Three",
-    name: "Name Three",
+    name: "Ahmed Salah",
+    avatar:
+      "https://images.squarespace-cdn.com/content/v1/5c2efbc95417fc1b3d3040ad/1623728514460-NKFH3X6TIK8JXUESNRQD/Modern+School+Portraits+Perth.jpg",
+    id: 2,
   },
   {
-    check: "Check One ",
-    name: "Name One",
+    name: "Kareem Momen",
+    avatar:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjh6VSzXG8EgTqDjm0JJ7CTlDVFnIHh6X9dPgBDqxt2kUOGgH7NQy0Ey6oPhEn5TVQbvI&usqp=CAU",
+    id: 3,
   },
   {
-    check: "Check Two",
-    name: "Name Two",
+    name: "Khaled Momen",
+    avatar:
+      "https://www.anthropics.com/portraitpro/img/page-images/homepage/v22/what-can-it-do-5B.jpg",
+    id: 3,
   },
   {
-    check: "Check Three",
-    name: "Name Three",
+    name: "mohammed Ali",
+    avatar:
+      "https://images.squarespace-cdn.com/content/v1/5c2efbc95417fc1b3d3040ad/1623735675824-LXS5FG198B0MLQCKQC0P/Modern+School+Photos.jpg",
+    id: 1,
   },
   {
-    check: "Check One ",
-    name: "Name One",
+    name: "Ahmed Salah",
+    avatar:
+      "https://images.squarespace-cdn.com/content/v1/5c2efbc95417fc1b3d3040ad/1623728514460-NKFH3X6TIK8JXUESNRQD/Modern+School+Portraits+Perth.jpg",
+    id: 2,
   },
   {
-    check: "Check Two",
-    name: "Name Two",
+    name: "Kareem Momen",
+    avatar:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjh6VSzXG8EgTqDjm0JJ7CTlDVFnIHh6X9dPgBDqxt2kUOGgH7NQy0Ey6oPhEn5TVQbvI&usqp=CAU",
+    id: 3,
   },
   {
-    check: "Check Three",
-    name: "Name Three",
+    name: "Khaled Momen",
+    avatar:
+      "https://www.anthropics.com/portraitpro/img/page-images/homepage/v22/what-can-it-do-5B.jpg",
+    id: 3,
   },
   {
-    check: "Check One ",
-    name: "Name One",
+    name: "mohammed Ali",
+    avatar:
+      "https://images.squarespace-cdn.com/content/v1/5c2efbc95417fc1b3d3040ad/1623735675824-LXS5FG198B0MLQCKQC0P/Modern+School+Photos.jpg",
+    id: 1,
   },
   {
-    check: "Check Two",
-    name: "Name Two",
+    name: "Ahmed Salah",
+    avatar:
+      "https://images.squarespace-cdn.com/content/v1/5c2efbc95417fc1b3d3040ad/1623728514460-NKFH3X6TIK8JXUESNRQD/Modern+School+Portraits+Perth.jpg",
+    id: 2,
   },
   {
-    check: "Check Three",
-    name: "Name Three",
+    name: "Kareem Momen",
+    avatar:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjh6VSzXG8EgTqDjm0JJ7CTlDVFnIHh6X9dPgBDqxt2kUOGgH7NQy0Ey6oPhEn5TVQbvI&usqp=CAU",
+    id: 3,
   },
   {
-    check: "Check One ",
-    name: "Name One",
+    name: "Khaled Momen",
+    avatar:
+      "https://www.anthropics.com/portraitpro/img/page-images/homepage/v22/what-can-it-do-5B.jpg",
+    id: 3,
   },
   {
-    check: "Check Two",
-    name: "Name Two",
+    name: "mohammed Ali",
+    avatar:
+      "https://images.squarespace-cdn.com/content/v1/5c2efbc95417fc1b3d3040ad/1623735675824-LXS5FG198B0MLQCKQC0P/Modern+School+Photos.jpg",
+    id: 1,
   },
   {
-    check: "Check Three",
-    name: "Name Three",
+    name: "Ahmed Salah",
+    avatar:
+      "https://images.squarespace-cdn.com/content/v1/5c2efbc95417fc1b3d3040ad/1623728514460-NKFH3X6TIK8JXUESNRQD/Modern+School+Portraits+Perth.jpg",
+    id: 2,
   },
   {
-    check: "Check One ",
-    name: "Name One",
+    name: "Kareem Momen",
+    avatar:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjh6VSzXG8EgTqDjm0JJ7CTlDVFnIHh6X9dPgBDqxt2kUOGgH7NQy0Ey6oPhEn5TVQbvI&usqp=CAU",
+    id: 3,
   },
   {
-    check: "Check Two",
-    name: "Name Two",
+    name: "Khaled Momen",
+    avatar:
+      "https://www.anthropics.com/portraitpro/img/page-images/homepage/v22/what-can-it-do-5B.jpg",
+    id: 3,
   },
   {
-    check: "Check Three",
-    name: "Name Three",
+    name: "mohammed Ali",
+    avatar:
+      "https://images.squarespace-cdn.com/content/v1/5c2efbc95417fc1b3d3040ad/1623735675824-LXS5FG198B0MLQCKQC0P/Modern+School+Photos.jpg",
+    id: 1,
   },
   {
-    check: "Check One ",
-    name: "Name One",
+    name: "Ahmed Salah",
+    avatar:
+      "https://images.squarespace-cdn.com/content/v1/5c2efbc95417fc1b3d3040ad/1623728514460-NKFH3X6TIK8JXUESNRQD/Modern+School+Portraits+Perth.jpg",
+    id: 2,
   },
   {
-    check: "Check Two",
-    name: "Name Two",
+    name: "Kareem Momen",
+    avatar:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjh6VSzXG8EgTqDjm0JJ7CTlDVFnIHh6X9dPgBDqxt2kUOGgH7NQy0Ey6oPhEn5TVQbvI&usqp=CAU",
+    id: 3,
   },
   {
-    check: "Check Three",
-    name: "Name Three",
-  },
-  {
-    check: "Check One ",
-    name: "Name One",
-  },
-  {
-    check: "Check Two",
-    name: "Name Two",
-  },
-  {
-    check: "Check Three",
-    name: "Name Three",
+    name: "Khaled Momen",
+    avatar:
+      "https://www.anthropics.com/portraitpro/img/page-images/homepage/v22/what-can-it-do-5B.jpg",
+    id: 3,
   },
 ];
-
-const rows = elements.map((element, index) => (
-  <>
-    <td onClick={() => console.log("Cheked")}>
-      {" "}
-      <Checkbox />{" "}
-    </td>
-  </>
-));
-
-const days = new Array(30).fill(15);
-console.log("Days", days);
 
 const AttendanceTable = (props: Props) => {
   return (
     <div className="overflow-scroll max-h-screen relative m-6 bg-white rounded-lg text-center">
-      <Table highlightOnHover>
+      <Table highlightOnHover horizontalSpacing="xl">
         <thead>
           <tr className="">
             <th className="bg-white sticky  top-0 z-20 ">Day</th>
-            {elements.map((element) => (
-              <th className="bg-white sticky top-0 z-20">{element.name}</th>
+            {players.map((player) => (
+              <th className="bg-white sticky top-0 z-20 text-center ">
+                <div className="flex  flex-col justify-center items-center">
+                  <Avatar radius={"xl"} size="md" src={player.avatar} />
+                  <span>{player.name}</span>
+                </div>
+              </th>
             ))}
           </tr>
         </thead>
         <tbody className="overflow-scroll">
-          {new Array(30).fill(1).map((item, idx) => {
-            console.log(item);
+          {new Array(30).fill(5).map((item, idx) => {
+            const thisDate = new Date(`${item}/${idx + 1}/2022`);
             return (
               <tr className="">
-                <td className=" sticky left-0 bg-white z-10">
-                  Friday {item}/{idx}/2022
+                <td className="text-sm w-32 sticky left-0 bg-white z-10 text-perfGray2">
+                  {thisDate.getDate()}/ {thisDate.getMonth()} /
+                  {thisDate.getFullYear()}
                 </td>
-                {elements.map((element, index) => (
+                {players.map((player, index) => (
                   <>
-                    <td
-                      onClick={() =>
-                        console.log("Cheked Item", element.check, " && ", idx)
-                      }
-                    >
-                      <Checkbox />
+                    <td>
+                      <Checkbox
+                        onChange={(e) => {
+                          console.log({
+                            attended: e.currentTarget.checked,
+                            plaerId: player.id,
+                            plaerName: player.name,
+                            date: new Date(`${item}/${idx}/2022`),
+                          });
+                        }}
+                      />
                     </td>
                   </>
                 ))}
