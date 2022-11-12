871b2ce580f2c7c932ecc8fd1ac8ef81c7942961
MAlngaawy <m.alngaawy@gmail.com>
2022-11-12 17:05:16 +0200

    handle reports page loading

---
+++ src/app/pages/media/molecules/MediaPageLoading.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Grid, Skeleton } from "@mantine/core";
+
+type Props = {};
+
+const MediaPageLoading = (props: Props) => {
+  return (
+    <Grid gutter={10}>
+      <Grid.Col xs={6} sm={4} md={3}>
+        <Skeleton height={300} width="100%" radius="lg" />
+      </Grid.Col>
+      <Grid.Col xs={6} sm={4} md={3}>
+        <Skeleton height={300} width="100%" radius="lg" />
+      </Grid.Col>
+      <Grid.Col xs={6} sm={4} md={3}>
+        <Skeleton height={300} width="100%" radius="lg" />
+      </Grid.Col>
+      <Grid.Col xs={6} sm={4} md={3}>
+        <Skeleton height={300} width="100%" radius="lg" />
+      </Grid.Col>
+      <Grid.Col xs={6} sm={4} md={3}>
+        <Skeleton height={300} width="100%" radius="lg" />
+      </Grid.Col>
+      <Grid.Col xs={6} sm={4} md={3}>
+        <Skeleton height={300} width="100%" radius="lg" />
+      </Grid.Col>
+      <Grid.Col xs={6} sm={4} md={3}>
+        <Skeleton height={300} width="100%" radius="lg" />
+      </Grid.Col>
+    </Grid>
+  );
+};
+
+export default MediaPageLoading;
