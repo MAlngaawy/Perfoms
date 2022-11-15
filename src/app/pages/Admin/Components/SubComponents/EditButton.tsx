a5408044dcc1d18ea221f5c800faeea2041bcce4
MAlngaawy <m.alngaawy@gmail.com>
2022-11-15 11:15:37 +0200

    separate team card in a fiel

---
+++ src/app/pages/Admin/Components/SubComponents/EditButton.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const EditButton = (props: Props) => {
+  return <div>EditButton</div>;
+};
+
+export default EditButton;
---
+++ src/app/pages/Admin/Components/SubComponents/TeamCard.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import DeleteButton from "./DeleteButton";
+
+type Props = {
+  team: {
+    id: number;
+    icon: string;
+    name: string;
+    sport: string;
+    age: { from: number; to: number };
+    players: number;
+  };
+};
+
+const TeamCard = ({ team }: Props) => {
+  return (
+    <div className="team-card relative w-72 bg-white p-8 rounded-xl flex flex-col justify-center items-center gap-4">
+      <Link
+        to={`teams/${team.id}`}
+        className="bg-pagesBg rounded-full w-32 h-32 flex justify-center items-center"
+      >
+        <img className="w-4/5" src={team.icon} alt="icon" />
+      </Link>
+
+      <h2 className="text-xl font-semibold text-perfGray1">{team.name}</h2>
+      <h3 className="text-xl text-perfBlue">{team.sport}</h3>
+
+      <div className="flex justify-around items-center w-full">
+        <div className="age flex flex-col">
+          <span className=" text-sm text-perfGray3">Age</span>
+          <span className="text-xl text-perfGray1">
+            {team.age.from} - {team.age.to}
+          </span>
+        </div>
+        <div className="players flex flex-col">
+          <span className=" text-sm text-perfGray3">Players</span>
+          <span className="text-xl text-perfGray1">{team.players}</span>
+        </div>
+      </div>
+
+      {/* Edit and Delete Buttons */}
+      <div className="flex absolute right-5 top-5">
+        <DeleteButton teamName={team.name} teamId={team.id} />
+      </div>
+    </div>
+  );
+};
+
+export default TeamCard;
