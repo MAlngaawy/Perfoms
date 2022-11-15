ac4a70d3ec9c4e3e4c33450adeb01748ff6f4466
MAlngaawy <m.alngaawy@gmail.com>
2022-11-15 16:40:26 +0200

    handle showing the teams page

--- src/app/pages/Admin/AdminPage.tsx
+++ src/app/pages/Admin/AdminPage.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import AppRadioGroub from "~/@main/components/AppRadioGroub";
+import Teams from "./Components/Teams";
 
 type Props = {};
 
@@ -17,6 +18,35 @@ const AdminPage = (props: Props) => {
         {/* <Button onClick={() => setChecked("Attendance")}>Attendances</Button>
         <Button onClick={() => setChecked("Performance")}>Performance</Button>
         <Button onClick={() => setChecked("Team info")}>Team info</Button> */}
+      </div>
+
+      <div className={checked !== "Teams" ? "hidden" : "block"}>
+        <Teams
+          data={[
+            {
+              id: 1,
+              icon: "https://freepngimg.com/thumb/football/1-football-ball-png-image-thumb.png",
+              name: "14Th Team",
+              sport: "Taekwondo",
+              age: { from: 12, to: 15 },
+              players: 30,
+            },
+            {
+              id: 2,
+              icon: "https://freepngimg.com/thumb/football/1-football-ball-png-image-thumb.png",
+              name: "14Th Team",
+              sport: "Taekwondo",
+              age: { from: 15, to: 18 },
+              players: 15,
+            },
+          ]}
+        />
+      </div>
+      <div className={checked !== "Sports" ? "hidden" : "block"}>
+        <h1>Sports Here</h1>
+      </div>
+      <div className={checked !== "Users" ? "hidden" : "block"}>
+        <h1>Uers Here</h1>
       </div>
     </div>
   );
