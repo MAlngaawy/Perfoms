974437168fc68b6943d26f15d49ce8c3a3be3e92
MAlngaawy <m.alngaawy@gmail.com>
2022-11-15 21:28:14 +0200

    edit some sttorybook issues

--- src/stories/CoachPersonalInfo.stories.tsx
+++ src/stories/CoachPersonalInfo.stories.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { Grid } from "@mantine/core";
-import CoachPersonalInfo from "../app/pages/coaches/SingleCoach/components/CoachPersonalInfo";
+import CoachPersonalInfo from "~/@main/components/CoachPersonalInfo";
 
 export default {
   title: "Cards/Coah Info",
