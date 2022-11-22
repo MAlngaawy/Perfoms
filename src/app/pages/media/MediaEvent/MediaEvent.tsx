859c627882e8a0b37b676261c3316cc1192aba8e
omarfarids <omar.farid007@gmail.com>
2022-11-22 06:29:32 +0200

    Media page: add event button and funcionality + add media button and functionality + fixing responsive issues

--- src/app/pages/media/MediaEvent/MediaEvent.tsx
+++ src/app/pages/media/MediaEvent/MediaEvent.tsx
@@ -1,33 +1,174 @@
-import React , { useState } from 'react'
-import { Card, Image, SimpleGrid } from '@mantine/core';
-import AppIcons from '~/@main/core/AppIcons';
-import { Carousel, useAnimationOffsetEffect } from '@mantine/carousel';
-import FirstNav from '~/@main/components/FirstNav';
+import React, { useState, useEffect } from "react";
+import { Card, Image, SimpleGrid } from "@mantine/core";
+import AppIcons from "~/@main/core/AppIcons";
+import { Carousel, useAnimationOffsetEffect } from "@mantine/carousel";
+import { useLocation } from "react-router-dom";
+import { Button } from "~/@main/components/Button";
+import {
+  Modal,
+  FileButton,
+  Button as MantineButton,
+  Group,
+  Text,
+  List,
+} from "@mantine/core";
 
+const TRANSITION_DURATION = 200;
 
-
-const MediaEvent = ({images}:{images:string[]}) => {
-  const TRANSITION_DURATION = 200;
+const MediaEvent = ({ images }: { images: string[] }) => {
   const [embla, setEmbla] = useState<any>(null);
+  const [opened, setOpened] = useState(false);
+  const [files, setFiles] = useState<File[]>([]);
+  const [isFocused, setIsFocused] = useState(false);
+  const [youtubeLink, setYoutubeLink] = useState("");
+  const [videoID, setVideoID] = useState("");
+
+  const location = useLocation();
 
   useAnimationOffsetEffect(embla, TRANSITION_DURATION);
 
+  const handleSaveClick = () => {
+    setOpened(false);
+    console.log(files);
+    console.log(youtubeLink);
+  };
 
-  const slides = images.map((url) => (
-    <Carousel.Slide className='bg-white' key={url}>
+  const handleYoutubeLinkInput = () => {
+    if (youtubeLink.indexOf("=") !== -1) {
+      if (youtubeLink.indexOf("&") !== -1) {
+        setVideoID(
+          youtubeLink.slice(
+            youtubeLink.indexOf("=") + 1,
+            youtubeLink.indexOf("&")
+          )
+        );
+      } else {
+        setVideoID(youtubeLink.slice(youtubeLink.indexOf("=") + 1));
+      }
+    }
+  };
+
+  useEffect(() => {
+    handleYoutubeLinkInput();
+  }, [youtubeLink]);
+
+  const slides = images.map((url, index) => (
+    <Carousel.Slide className="bg-white" key={index}>
       <Image src={url} />
     </Carousel.Slide>
   ));
 
   return (
     <>
-        <FirstNav pageName='event' />
-        <Carousel controlsOffset="xl" controlSize={36} className='m-auto mt-10' sx={{ width: '50%' , minWidth:300 }} withIndicators loop getEmblaApi={setEmbla}>
-          {slides}
-        </Carousel>
+      <Carousel
+        controlsOffset="xl"
+        controlSize={36}
+        className="mx-auto mt-10"
+        sx={{ width: "50%", minWidth: 300 }}
+        withIndicators
+        loop
+        getEmblaApi={setEmbla}
+      >
+        {slides}
+      </Carousel>
+      <div className="hidden xs:flex flex-row justify-between xs:w-1/2 mx-auto pt-3 items-start">
+        <div className="flex flex-col gap-1 text-perfGray2 text-sm font-medium">
+          <p className="text-lg md:text-2xl pb-1 text-black">
+            {location.state.header}
+          </p>
+          <p>
+            <AppIcons className="w-5 inline" icon="CalendarIcon:outline" />{" "}
+            {location.state.date}
+          </p>
+          <p>
+            <AppIcons className="w-5 inline" icon="MapIcon:outline" />{" "}
+            {location.state.place}
+          </p>
+        </div>
+        <Button
+          label="Add Event"
+          onClick={() => setOpened(true)}
+          style="bordered"
+          className="w-32 mx-0 mt-0 h-8 rounded-full"
+          icon="plus icon"
+        />
+        <Modal
+          sx={{
+            ".mantine-Modal-modal": {
+              height: "100%",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "stretch",
+              padding: "0 50px",
+            },
+          }}
+          withCloseButton={false}
+          radius="xl"
+          padding="xl"
+          opened={opened}
+          onClose={() => setOpened(false)}
+        >
+          {videoID && (
+            <iframe
+              className="w-52 h-32 mx-auto"
+              src={`https://www.youtube.com/embed/${videoID}`}
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              title="Embedded youtube"
+            />
+          )}
+          <input
+            value={youtubeLink}
+            onChange={(e) => setYoutubeLink(e.target.value)}
+            className="w-full h-12 my-5 text-center rounded-lg border border-perfBlue text-perfBlue focus:border-2 placeholder:content-['yye'] placeholder:text-center placeholder:text-perfBlue placeholder:font-medium placeholder:text-sm pl-3"
+            placeholder={`${
+              isFocused ? "www.youtube.com" : "Add Youtube link here.."
+            }`}
+            onFocus={() => setIsFocused(true)}
+            onBlur={() => setIsFocused(false)}
+          />
+          <Group position="center">
+            <FileButton
+              onChange={setFiles}
+              accept="image/png,image/jpeg"
+              multiple
+            >
+              {(props) => (
+                <MantineButton
+                  className="w-full h-12 rounded-lg border border-perfBlue text-perfBlue hover:shadow-lg hover:bg-white"
+                  {...props}
+                >
+                  Upload image
+                </MantineButton>
+              )}
+            </FileButton>
+          </Group>
+          {files.length > 0 && <Text size="sm" mt="sm"></Text>}
+
+          <List size="sm" mt={5} withPadding>
+            {files.map((file, index) => (
+              <List.Item key={index}>
+                {file.name}{" "}
+                <AppIcons
+                  className="w-4 font-medium text-green inline"
+                  icon="CheckIcon:outline"
+                />
+              </List.Item>
+            ))}
+          </List>
+
+          <Button
+            label="Save"
+            style="primary"
+            onClick={handleSaveClick}
+            className="w-full h-12 rounded-lg my-5"
+          />
+        </Modal>
+      </div>
     </>
   );
+};
 
-}
-
-export default MediaEvent
+export default MediaEvent;
--- src/app/pages/media/MediaPage.tsx
+++ src/app/pages/media/MediaPage.tsx
@@ -3,6 +3,10 @@ import FirstNav from "~/@main/components/FirstNav";
 import SecondNav from "../home/organisms/SecondNav";
 import MediaCard from "./molecules/MediaCard";
 import { players } from "../home/HomePage";
+import AppIcons from "~/@main/core/AppIcons";
+import { Modal } from "@mantine/core";
+import { Dropdown } from "~/@main/components/Dropdown";
+import { Button } from "~/@main/components/Button";
 
 const dummyData = [
   {
@@ -68,31 +72,77 @@ const dummyData = [
     date: "Sunday, 15/SEP",
     place: "Al - ahly club",
   },
-  {
-    id: 9,
-    img: "https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80",
-    header: "Kickboxing -under 12",
-    date: "Sunday, 15/SEP",
-    place: "Al - ahly club",
-  },
 ];
+
+const user_type = "Parent";
+
 const MediaPage = () => {
-  const [selectedplayer, setSelectedPlayer] = useState<any>(null);
+  const [opened, setOpened] = useState(false);
+  const [select, setSeleced] = useState("Event Name");
+  const [events, setEvents] = useState(dummyData);
+
+  const handleClick = () => {
+    setOpened(false);
+    setEvents([
+      ...events,
+      {
+        id: events.length,
+        img: "https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80",
+        header: "Kickboxing -under 12",
+        date: "Sunday, 15/SEP",
+        place: "Al - ahly club",
+      },
+    ]);
+  };
 
   return (
     <div>
-      <FirstNav pageName="Media" />
-      {/* <SecondNav
-          players={players}
-          selectedplayer={selectedplayer}
-          setSelectedPlayer={setSelectedPlayer}
-        /> */}
-      <div className="flex flex-row justify-center items-center flex-wrap gap-2 m-1">
-        {dummyData.map((data) => {
-          return <MediaCard {...data} />;
+      <div className="flex flex-col xs:flex-row justify-center xs:items-center flex-wrap gap-2 m-1">
+        {events.map((data) => {
+          return <MediaCard key={data.id} {...data} />;
         })}
+        {user_type === "Parent" && (
+          <button
+            onClick={() => setOpened(true)}
+            className="rounded-xl flex justify-center items-center flex-col bg-perfLightGray hover:shadow-xl shadow self-stretch xs:w-52 px-1 box-content h-80"
+          >
+            <AppIcons className="w-12 text-perfGray3" icon="PlusIcon:outline" />{" "}
+            <span className="text-lg font-medium text-perfGray3">
+              Add event
+            </span>
+          </button>
+        )}
       </div>
-      ;
+      <Modal
+        sx={{
+          ".mantine-Modal-modal": {
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          },
+        }}
+        withCloseButton={false}
+        radius="xl"
+        padding="xl"
+        opened={opened}
+        onClose={() => setOpened(false)}
+      >
+        <Dropdown
+          selected={select}
+          setSelected={setSeleced}
+          values={["Event1", "Event2"]}
+          className="w-96 text-lg font-medium flex-row justify-between"
+        />
+        <div className="border w-80 mx-auto mt-5" />
+        <Button
+          label="Add Media"
+          onClick={handleClick}
+          style="primary"
+          className="w-full h-12 mt-10 rounded-lg"
+        />
+      </Modal>
     </div>
   );
 };
