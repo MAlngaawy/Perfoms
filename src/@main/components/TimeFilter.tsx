57893f1dc0a212457bd92bda9d478e9728763555
MAlngaawy <m.alngaawy@gmail.com>
2022-11-26 20:18:23 +0200

    create the time ffilter compoent

--- src/@main/components/TimeFilter.tsx
+++ src/@main/components/TimeFilter.tsx
@@ -3,6 +3,7 @@ import { RangeCalendar } from "@mantine/dates";
 import { Menu, Button } from "@mantine/core";
 import AppIcons from "./../../@main/core/AppIcons";
 import useWindowSize from "../hooks/useWindowSize";
+import classNames from "classnames";
 
 type Props = {};
 
@@ -11,9 +12,10 @@ const thisWeek = () => {
   var first = curr.getDate() - curr.getDay(); // First day is the day of the month - the day of the week
   var last = first + 6; // last day is the first day + 6
 
-  var firstday = new Date(curr.setDate(first)).toUTCString();
-  var lastday = new Date(curr.setDate(last)).toUTCString();
-  console.log([firstday, lastday]);
+  return {
+    firstday: new Date(curr.setDate(first)),
+    lastday: new Date(curr.setDate(last)),
+  };
 };
 
 const lastWeek = () => {
@@ -21,9 +23,12 @@ const lastWeek = () => {
   var first = curr.getDate() - curr.getDay() - 7; // First day is the day of the month - the day of the week
   var last = first + 6; // last day is the first day + 6
 
-  var firstday = new Date(curr.setDate(first)).toUTCString();
-  var lastday = new Date(curr.setDate(last)).toUTCString();
-  console.log([firstday, lastday]);
+  var firstday = new Date(curr.setDate(first));
+  var lastday = new Date(curr.setDate(last));
+  return {
+    firstday,
+    lastday,
+  };
 };
 
 const last2Weeks = () => {
@@ -31,39 +36,45 @@ const last2Weeks = () => {
   var first = curr.getDate() - curr.getDay() - 14; // First day is the day of the month - the day of the week
   var last = first + 13; // last day is the first day + 6
 
-  var firstday = new Date(curr.setDate(first)).toUTCString();
-  var lastday = new Date(curr.setDate(last)).toUTCString();
-  console.log([firstday, lastday]);
+  var firstday = new Date(curr.setDate(first));
+  var lastday = new Date(curr.setDate(last));
+  return {
+    firstday,
+    lastday,
+  };
 };
 
 const thisMonth = () => {
   let date = new Date(),
     y = date.getFullYear(),
     m = date.getMonth();
-  let firstDate = new Date(y, m, 1);
-  let lastDate = new Date(y, m + 1, 0);
-  console.log([firstDate, lastDate]);
+  let firstday = new Date(y, m, 1);
+  let lastday = new Date(y, m + 1, 0);
+  return {
+    firstday,
+    lastday,
+  };
 };
 
 const lastMonth = () => {
   let date = new Date(),
     y = date.getFullYear(),
     m = date.getMonth() - 1;
-  let firstDate = new Date(y, m, 1);
-  let lastDate = new Date(y, m + 1, 0);
-  console.log([firstDate, lastDate]);
+  let firstday = new Date(y, m, 1);
+  let lastday = new Date(y, m + 1, 0);
+  return { firstday, lastday };
 };
 
 const thisYear = () => {
-  let firstDate = new Date(new Date().getFullYear(), 0, 1);
-  let lastDate = new Date(new Date().getFullYear(), 11, 31);
-  console.log([firstDate, lastDate]);
+  let firstday = new Date(new Date().getFullYear(), 0, 1);
+  let lastday = new Date(new Date().getFullYear(), 11, 31);
+  return { firstday, lastday };
 };
 
 const lastYear = () => {
-  let firstDate = new Date(new Date().getFullYear() - 1, 0, 1);
-  let lastDate = new Date(new Date().getFullYear() - 1, 11, 31);
-  console.log([firstDate, lastDate]);
+  let firstday = new Date(new Date().getFullYear() - 1, 0, 1);
+  let lastday = new Date(new Date().getFullYear() - 1, 11, 31);
+  return { firstday, lastday };
 };
 
 const TimeFilter = (props: Props) => {
@@ -98,16 +109,63 @@ const TimeFilter = (props: Props) => {
         </Menu.Target>
 
         <Menu.Dropdown>
-          <div className="flex flex-col gap-2">
-            <div className="dates flex flex-col items-center justify-center gap-2 border-r">
-              <Menu.Item className="p-2 cursor-pointer text-perfGray3 border-b">
-                DayOne
-              </Menu.Item>
+          <div className="flex flex-col sm:flex-row gap-2">
+            <div className="dates flex flex-col items-center justify-center gap-2 sm:border-r border-neutral-700">
+              <FilterType
+                type="This Week"
+                setTextValue={setTextValue}
+                setValue={setValue}
+                textValue={textValue}
+                filterFun={thisWeek}
+              />
+              <FilterType
+                type="Last Week"
+                setTextValue={setTextValue}
+                setValue={setValue}
+                textValue={textValue}
+                filterFun={lastWeek}
+              />
+              <FilterType
+                type="Last Two Week"
+                setTextValue={setTextValue}
+                setValue={setValue}
+                textValue={textValue}
+                filterFun={last2Weeks}
+              />
+              <FilterType
+                type="This Month"
+                setTextValue={setTextValue}
+                setValue={setValue}
+                textValue={textValue}
+                filterFun={thisMonth}
+              />
+              <FilterType
+                type="Last Month"
+                setTextValue={setTextValue}
+                setValue={setValue}
+                textValue={textValue}
+                filterFun={lastMonth}
+              />
+              <FilterType
+                type="This Year"
+                setTextValue={setTextValue}
+                setValue={setValue}
+                textValue={textValue}
+                filterFun={thisYear}
+              />
+              <FilterType
+                type="Last Year"
+                setTextValue={setTextValue}
+                setValue={setValue}
+                textValue={textValue}
+                filterFun={thisYear}
+              />
             </div>
-            <div className="calendar flex flex-col">
+
+            <div className="calendar">
               <RangeCalendar
                 amountOfMonths={
-                  windwSize.width && windwSize.width > 700 ? 2 : 1
+                  windwSize.width && windwSize.width > 768 ? 2 : 1
                 }
                 value={value}
                 onChange={setValue}
@@ -120,3 +178,36 @@ const TimeFilter = (props: Props) => {
   );
 };
 export default TimeFilter;
+
+const FilterType = ({
+  type,
+  setValue,
+  setTextValue,
+  textValue,
+  filterFun,
+}: {
+  type: any;
+  setValue: any;
+  setTextValue: any;
+  textValue: any;
+  filterFun: any;
+}) => {
+  return (
+    <span className="border-b w-full  ">
+      <Menu.Item
+        onClick={() => {
+          setValue([filterFun().firstday, filterFun().lastday]);
+          setTextValue(type);
+        }}
+        className={classNames(
+          "p-2 w-full text-left cursor-pointer text-perfGray3",
+          {
+            "bg-gray-100": textValue === type,
+          }
+        )}
+      >
+        <span>{type}</span>
+      </Menu.Item>
+    </span>
+  );
+};
