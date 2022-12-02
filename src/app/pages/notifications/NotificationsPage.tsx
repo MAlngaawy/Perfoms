3c0047e8c15205a48b8250d7b8d10942995f7dac
MAlngaawy <m.alngaawy@gmail.com>
2022-12-02 13:32:37 +0200

    handle notifications page

--- src/app/pages/notifications/NotificationsPage.tsx
+++ src/app/pages/notifications/NotificationsPage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import OneNotification from "./components/OneNotification";
+import { useNotificationsQuery } from "~/app/store/user/userApi";
 
 type Props = {};
 
@@ -35,11 +36,39 @@ const data = [
   },
 ];
 
+const formatDate = (date: Date | null) => {
+  if (date) {
+    const today = date;
+    const yyyy = today.getFullYear();
+    let mm: string | number = today.getMonth() + 1; // Months start at 0!
+    let dd: string | number = today.getDate();
+
+    if (dd < 10) dd = "0" + dd;
+    if (mm < 10) mm = "0" + mm;
+
+    return yyyy + "-" + mm + "-" + dd;
+  } else {
+    return "NA";
+  }
+};
+
 const NotificationsPage = (props: Props) => {
+  const { data: notifications } = useNotificationsQuery({});
+
+  console.log("notifications", notifications);
+
   return (
     <div className="p-6 flex flex-col gap-4">
-      {data.map((oneNotification) => (
-        <OneNotification {...oneNotification} />
+      {notifications?.results.map((oneNot) => (
+        <OneNotification
+          created_at={formatDate(new Date(oneNot.created_at))}
+          newNotification
+          notification_type={oneNot.notification_type}
+          senderAvatar={oneNot.sender.avatar}
+          senderName={
+            (oneNot.sender.full_name && oneNot.sender.full_name) || "Anonymos"
+          }
+        />
       ))}
     </div>
   );
