aa00f679ed9457d1cdddf6259f7c063aa8f539de
MAlngaawy <m.alngaawy@gmail.com>
2022-12-05 17:01:31 +0200

    add bead crimbs

--- src/app/pages/MainReports/SupPages/Players/SearchPlayersPage.tsx
+++ src/app/pages/MainReports/SupPages/Players/SearchPlayersPage.tsx
@@ -1,8 +1,9 @@
-import { Avatar, Group, Select, Text } from "@mantine/core";
+import { Avatar, Breadcrumbs, Group, Select, Text } from "@mantine/core";
 import React, { forwardRef, useEffect } from "react";
 import AppIcons from "~/@main/core/AppIcons";
 import { Controller, useForm } from "react-hook-form";
 import { useSuperPlayersQuery } from "~/app/store/supervisor/supervisorMainApi";
+import { Link } from "react-router-dom";
 
 type Props = {};
 
@@ -51,59 +52,75 @@ const SearchPlayersPage = (props: Props) => {
     )
   );
 
-  return (
-    <div className="bg-white rounded-xl flex flex-col gap-8 mt-20 justify-center items-center py-9 px-4 w-11/12 sm:w-8/12 mx-auto">
-      <h2 className="text-xl text-perfGray1 font-semibold">
-        Search for a player by name or phone number
-      </h2>
-      <form
-        className="flex w-11/12 justify-center"
-        onSubmit={handleSubmit(send)}
-      >
-        {data && (
-          <Controller
-            control={control}
-            {...register("id")}
-            render={({ field }) => (
-              <Select
-                id="id"
-                {...field}
-                data={data}
-                itemComponent={SelectItem}
-                icon={
-                  <AppIcons
-                    icon="MagnifyingGlassIcon:outline"
-                    className="w-5 h-5 text-perfGray1"
-                  />
-                }
-                sx={{
-                  ".mantine-Select-input": {
-                    background: "#eee",
-                    borderTopLeftRadius: 30,
-                    borderBottomLeftRadius: 30,
-                    margin: 0,
-                    padding: 0,
-                    paddingLeft: 30,
-                  },
-                  ".mantine-Select-rightSection": {
-                    display: "none",
-                  },
-                }}
-                rightSection={false}
-                className="m-0 p-0 h-fit w-8/12"
-                searchable
-              />
-            )}
-          />
-        )}
+  const items = [
+    { title: "categories", href: "/main-reports" },
+    { title: "Search Players", href: "" },
+  ].map((item, index) => (
+    <Link to={item.href} key={index}>
+      {item.title}
+    </Link>
+  ));
 
-        <button
-          type="submit"
-          className="bg-perfGray3 text-white px-2 rounded-tr-full rounded-br-full"
+  return (
+    <div className="container w-11/12 mx-auto">
+      <div className="my-4">
+        <Breadcrumbs className="text-perfGray3" separator="→">
+          {items}
+        </Breadcrumbs>
+      </div>
+      <div className="bg-white rounded-xl flex flex-col gap-8 mt-20 justify-center items-center py-9 px-4 w-11/12 sm:w-8/12 mx-auto">
+        <h2 className="text-xl text-perfGray1 font-semibold">
+          Search for a player by name or phone number
+        </h2>
+        <form
+          className="flex w-11/12 justify-center"
+          onSubmit={handleSubmit(send)}
         >
-          Search
-        </button>
-      </form>
+          {data && (
+            <Controller
+              control={control}
+              {...register("id")}
+              render={({ field }) => (
+                <Select
+                  id="id"
+                  {...field}
+                  data={data}
+                  itemComponent={SelectItem}
+                  icon={
+                    <AppIcons
+                      icon="MagnifyingGlassIcon:outline"
+                      className="w-5 h-5 text-perfGray1"
+                    />
+                  }
+                  sx={{
+                    ".mantine-Select-input": {
+                      background: "#eee",
+                      borderTopLeftRadius: 30,
+                      borderBottomLeftRadius: 30,
+                      margin: 0,
+                      padding: 0,
+                      paddingLeft: 30,
+                    },
+                    ".mantine-Select-rightSection": {
+                      display: "none",
+                    },
+                  }}
+                  rightSection={false}
+                  className="m-0 p-0 h-fit w-8/12"
+                  searchable
+                />
+              )}
+            />
+          )}
+
+          <button
+            type="submit"
+            className="bg-perfGray3 text-white px-2 rounded-tr-full rounded-br-full"
+          >
+            Search
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
