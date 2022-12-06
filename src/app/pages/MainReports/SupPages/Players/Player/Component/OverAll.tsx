3309c5254a68785c7c4954d206df561839068c63
MAlngaawy <m.alngaawy@gmail.com>
2022-12-06 16:25:41 +0200

    handle player page reports

---
+++ src/app/pages/MainReports/SupPages/Players/Player/Component/OverAll.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const OverAll = (props: Props) => {
+  return <div>NewReports</div>;
+};
+
+export default OverAll;
--- src/app/pages/MainReports/SupPages/Players/Player/Player.tsx
+++ src/app/pages/MainReports/SupPages/Players/Player/Player.tsx
@@ -1,12 +1,13 @@
-import { Breadcrumbs } from "@mantine/core";
-import React from "react";
+import React, { useState } from "react";
+import { Breadcrumbs, Switch } from "@mantine/core";
 import { Link, useParams } from "react-router-dom";
 
 type Props = {};
 
 const items = [
   { title: "categories", href: "/main-reports" },
-  { title: "Search Players", href: "" },
+  { title: "Search Players", href: "/main-reports/search-players" },
+  { title: "Players", href: "" },
 ].map((item, index) => (
   <Link to={item.href} key={index}>
     {item.title}
@@ -14,17 +15,34 @@ const items = [
 ));
 
 const Player = (props: Props) => {
+  const [checked, setChecked] = useState(false);
   const { id } = useParams();
   console.log(id);
 
   return (
     <div className="conatiner w-11/12 mx-auto">
-      <div className="my-4">
+      <div className="mt-4">
         <Breadcrumbs className="text-perfGray3" separator="→">
           {items}
         </Breadcrumbs>
       </div>
-      Player {id}
+      <div className="switch flex ">
+        <Switch
+          size="xl"
+          sx={{
+            ".mantine-Switch-track": {
+              cursor: "pointer",
+            },
+          }}
+          onLabel="Overall"
+          offLabel="Detailed"
+          checked={checked}
+          onChange={(event) => setChecked(event.currentTarget.checked)}
+        />
+      </div>
+      <div className="my-6">
+        {checked ? <div> New Charts </div> : <div> Old Charts </div>}
+      </div>
     </div>
   );
 };
