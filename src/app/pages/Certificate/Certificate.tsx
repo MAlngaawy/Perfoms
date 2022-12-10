cb7b843c4b47ac743a07aee61a96e54cfd53e79a
MAlngaawy <m.alngaawy@gmail.com>
2022-12-10 21:16:21 +0200

    add certificate bread crumps

--- src/app/pages/Certificate/Certificate.tsx
+++ src/app/pages/Certificate/Certificate.tsx
@@ -5,6 +5,7 @@ import { number } from "yup/lib/locale";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import SubmitButton from "~/@main/components/SubmitButton";
+import CustomBreadCrumbs from "~/@main/components/BreadCrumbs";
 
 type Props = {};
 
@@ -34,77 +35,85 @@ const Certificate = (props: Props) => {
   const by = watch("by");
 
   return (
-    <div className="flex justify-center items-center">
-      <form
-        onSubmit={handleSubmit(sendCertificate)}
-        className=" py-28 w-72 flex flex-col justify-center items-center gap-4"
-      >
-        <PerfSelect
-          {...register("by")}
-          id="by"
-          required
-          error={errors.by && "please select this certificate by what"}
-          className="w-full"
-          label="By"
-          name="by"
-          control={control}
-          data={[
-            { label: "By Player", value: "By Player" },
-            { label: "By Team", value: "By Team" },
-          ]}
-        />
-
-        {by === "By Player" && (
+    <div className="m-4">
+      <CustomBreadCrumbs
+        items={[
+          { title: "Home", href: "/coach-home" },
+          { title: "Certificate", href: "" },
+        ]}
+      />
+      <div className="flex justify-center items-center">
+        <form
+          onSubmit={handleSubmit(sendCertificate)}
+          className=" py-28 w-72 flex flex-col justify-center items-center gap-4"
+        >
           <PerfSelect
-            {...register("player")}
-            id="player"
+            {...register("by")}
+            id="by"
             required
-            error={errors.by && "please select the player"}
+            error={errors.by && "please select this certificate by what"}
             className="w-full"
-            label="Player"
-            name="player"
+            label="By"
+            name="by"
             control={control}
             data={[
-              { label: "PLayer One", value: "PLayer One" },
-              { label: "Player Two", value: "Player Two" },
+              { label: "By Player", value: "By Player" },
+              { label: "By Team", value: "By Team" },
             ]}
           />
-        )}
 
-        {by === "By Team" && (
+          {by === "By Player" && (
+            <PerfSelect
+              {...register("player")}
+              id="player"
+              required
+              error={errors.by && "please select the player"}
+              className="w-full"
+              label="Player"
+              name="player"
+              control={control}
+              data={[
+                { label: "PLayer One", value: "PLayer One" },
+                { label: "Player Two", value: "Player Two" },
+              ]}
+            />
+          )}
+
+          {by === "By Team" && (
+            <PerfSelect
+              {...register("team")}
+              id="team"
+              required
+              error={errors.by && "please select the team"}
+              className="w-full"
+              label="Team"
+              name="team"
+              control={control}
+              data={[
+                { label: "Team One", value: "Team One" },
+                { label: "Team Two", value: "Team Two" },
+              ]}
+            />
+          )}
+
           <PerfSelect
-            {...register("team")}
-            id="team"
+            {...register("type")}
+            id="type"
             required
-            error={errors.by && "please select the team"}
+            error={errors.by && "please select the type of certificate"}
             className="w-full"
-            label="Team"
-            name="team"
+            label="Type Of Certificate"
+            name="type"
             control={control}
             data={[
-              { label: "Team One", value: "Team One" },
-              { label: "Team Two", value: "Team Two" },
+              { label: "Performances", value: "Performances" },
+              { label: "Attendances", value: "Attendances" },
             ]}
           />
-        )}
 
-        <PerfSelect
-          {...register("type")}
-          id="type"
-          required
-          error={errors.by && "please select the type of certificate"}
-          className="w-full"
-          label="Type Of Certificate"
-          name="type"
-          control={control}
-          data={[
-            { label: "Performances", value: "Performances" },
-            { label: "Attendances", value: "Attendances" },
-          ]}
-        />
-
-        <SubmitButton isLoading={false} text="Save" />
-      </form>
+          <SubmitButton isLoading={false} text="Save" />
+        </form>
+      </div>
     </div>
   );
 };
