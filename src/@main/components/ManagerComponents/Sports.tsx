f0a21991e2bc06145b8f128084eeb5ce23923319
MAlngaawy <m.alngaawy@gmail.com>
2022-12-19 22:21:50 +0200

    handle fetching sports

--- src/@main/components/ManagerComponents/Sports.tsx
+++ src/@main/components/ManagerComponents/Sports.tsx
@@ -6,32 +6,47 @@ import DeleteButton from "./SubComponents/DeleteButton";
 import EditSport from "./SubComponents/EditSport";
 import { Avatar } from "@mantine/core";
 import { useSuperSportQuery } from "~/app/store/supervisor/supervisorMainApi";
+import { useAdminSportsQuery } from "~/app/store/clubManager/clubManagerApi";
 
 type Props = {};
 
 const Sports = (props: Props) => {
   const { data: sport } = useSuperSportQuery({});
+  const { data: sports } = useAdminSportsQuery({});
+
   return (
     <div className="admin-teams flex flex-col xs:flex-row flex-wrap items-stretch gap-4 p-2 sm:p-6">
-      {/* {sports.map((sport) => { */}
-      {/* return ( */}
-      <div className="sport-card relative bg-white rounded-3xl p-12 flex flex-col justify-center items-center gap-4">
-        <Link
-          to={`sports/1`}
-          className="bg-pagesBg rounded-full w-24 h-24 flex justify-center items-center"
-        >
-          <Avatar size={"xl"} src={sport?.icon_url} alt="icon" />
-        </Link>
-        <h2 className="text-xl text-perfBlue">{sport?.name}</h2>
-        {/* Edit and Delete Buttons */}
-        <div className="flex absolute right-2 top-5 gap-2">
-          {/* <EditSport sportName={sport.name} sportId={sport.id} />
+      {sports &&
+        sports?.results.map((sport) => {
+          return (
+            <div className="sport-card relative bg-white rounded-3xl p-12 flex flex-col justify-center items-center gap-4">
+              <Link
+                to={`sports/${sport.id}`}
+                className="bg-pagesBg rounded-full w-24 h-24 flex justify-center items-center"
+              >
+                <Avatar size={"xl"} src={sport?.icon_url} alt="icon" />
+              </Link>
+              <h2 className="text-xl text-perfBlue">{sport?.name}</h2>
+              {/* Edit and Delete Buttons */}
+              <div className="flex absolute right-2 top-5 gap-2">
+                {/* <EditSport sportName={sport.name} sportId={sport.id} />
               <DeleteButton name={sport.name} id={sport.id} type="Sport" /> */}
+              </div>
+            </div>
+          );
+        })}
+      {sport && (
+        <div className="sport-card relative bg-white rounded-3xl p-12 flex flex-col justify-center items-center gap-4">
+          <Link
+            to={`sports/${sport.id}`}
+            className="bg-pagesBg rounded-full w-24 h-24 flex justify-center items-center"
+          >
+            <Avatar size={"xl"} src={sport?.icon_url} alt="icon" />
+          </Link>
+          <h2 className="text-xl text-perfBlue">{sport?.name}</h2>
         </div>
-      </div>
-      {/* ); */}
-      {/* })} */}
-      {/* <AddSport /> */}
+      )}
+      {sports && <AddSport />}
     </div>
   );
 };
