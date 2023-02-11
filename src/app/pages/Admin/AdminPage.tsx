2f8628c74d7ffbb28860cd2837ef1adf2cfa1f7a
MAlngaawy <m.alngaawy@gmail.com>
2023-02-11 15:23:52 +0200

    showing sports first

--- src/app/pages/Admin/AdminPage.tsx
+++ src/app/pages/Admin/AdminPage.tsx
@@ -7,13 +7,14 @@ import Users from "../../../@main/components/ManagerComponents/Users";
 type Props = {};
 
 const AdminPage = (props: Props) => {
-  const [checked, setChecked] = useState<"Teams" | "Sports" | "Users">("Teams");
+  const [checked, setChecked] =
+    useState<"Teams" | "Sports" | "Users">("Sports");
 
   return (
     <div>
       <div className="flex gap-2 xs:gap-4  p-2 sm:p-6 sm:pb-0">
         <AppRadioGroub
-          values={["Teams", "Sports", "Users"]}
+          values={["Sports", "Teams", "Users"]}
           checked={checked}
           setChecked={setChecked}
         />
@@ -22,11 +23,11 @@ const AdminPage = (props: Props) => {
         <Button onClick={() => setChecked("Team info")}>Team info</Button> */}
       </div>
 
-      <div className={checked !== "Teams" ? "hidden" : "block"}>
-        <Teams />
-      </div>
       <div className={checked !== "Sports" ? "hidden" : "block"}>
         <Sports />
+      </div>
+      <div className={checked !== "Teams" ? "hidden" : "block"}>
+        <Teams />
       </div>
       <div className={checked !== "Users" ? "hidden" : "block"}>
         <h1>
