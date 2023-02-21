32375739cd886a8e8b41f59236229ada0225caa9
MAlngaawy <m.alngaawy@gmail.com>
2023-02-21 17:50:21 +0200

    handle access token

--- src/app/auth/AuthContext.tsx
+++ src/app/auth/AuthContext.tsx
@@ -3,12 +3,17 @@ import AppSplashScreen from "~/@main/core/AppSplashScreen";
 import { useUserQuery } from "~/app/store/user/userApi";
 import { eventInstance } from "~/@main/utils/AppUtils";
 import { dataServerToken } from "../configs/dataService";
+import Cookies from "js-cookie";
 
 const AuthContext = createContext({});
 
 function AuthProvider({ children }: PropsWithChildren) {
   const { data, isLoading, isError, isSuccess, error, isFetching, refetch } =
     useUserQuery(null);
+
+  console.log("test dataServerToken", dataServerToken);
+  console.log("test cookies", Cookies.get("token"));
+  console.log("test Equal", dataServerToken === Cookies.get("token"));
 
   useEffect(() => {
     eventInstance.on("Login_Success", () => refetch());
@@ -17,8 +22,10 @@ function AuthProvider({ children }: PropsWithChildren) {
     /************************************************************ */
     /* This if condition I have made to refresh the page after login
      to make the dataservice.ts file to access token after it changes */
+
+    /**SomeTimes the  dataServerToken become null and sometimes it become an old token*/
     if (data) {
-      if (!dataServerToken) {
+      if (dataServerToken !== Cookies.get("token")) {
         location.reload();
       }
     } else {
