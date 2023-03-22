8fd3af58512b172869a608a0486c95aa33ba7e37
MAlngaawy <m.alngaawy@gmail.com>
2023-03-22 16:35:07 +0200

    using lazy loading in importing components

--- src/app/pages/coachHome/CoachHome.tsx
+++ src/app/pages/coachHome/CoachHome.tsx
@@ -1,13 +1,16 @@
-import { useState, useEffect } from "react";
-import AppRadioGroub from "../../../@main/components/AppRadioGroub";
-import AttendanceTable from "./components/AttendanceTable";
-import PerformanceTable from "./components/PerformanceTable";
-import TeamInfo from "./components/TeamInfo";
-import { Button } from "@mantine/core";
+import { useState, lazy, Suspense } from "react";
+import { Button, Skeleton } from "@mantine/core";
 import { Link } from "react-router-dom";
-import TeamFilter from "~/@main/components/TeamFilter";
 import classNames from "classnames";
+import AppRadioGroub from "../../../@main/components/AppRadioGroub";
+import TeamFilter from "~/@main/components/TeamFilter";
 import SwitchButton from "~/@main/components/SwitchButton";
+
+const LazyAttendanceTable = lazy(() => import("./components/AttendanceTable"));
+const LazyPerformanceTable = lazy(
+  () => import("./components/PerformanceTable")
+);
+const LazyTeamInfo = lazy(() => import("./components/TeamInfo"));
 
 type Props = {};
 
@@ -15,15 +18,31 @@ const CoachHome = (props: Props) => {
   const [checked, setChecked] =
     useState<"Attendance" | "Performance" | "Team info">("Attendance");
 
+  const renderTable = () => {
+    switch (checked) {
+      case "Attendance":
+        return <LazyAttendanceTable />;
+      case "Performance":
+        return (
+          <Suspense fallback={<SkelatonComponent />}>
+            <LazyPerformanceTable />
+          </Suspense>
+        );
+      case "Team info":
+        return (
+          <Suspense fallback={<SkelatonComponent />}>
+            <LazyTeamInfo />
+          </Suspense>
+        );
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className="coach-home">
       <div className="flex flex-col sm:flex-row items-center justify-between gap-4 m-4 ">
         <div className="flex items-center flex-wrap gap-1 xs:gap-4">
-          {/* <AppRadioGroub
-            values={["Attendance", "Performance", "Team info"]}
-            checked={checked}
-            setChecked={setChecked}
-          /> */}
           <SwitchButton
             checked={checked}
             setChecked={setChecked}
@@ -39,25 +58,29 @@ const CoachHome = (props: Props) => {
             setChecked={setChecked}
             type={"Team info"}
           />
-
-          <button className="border text-perfBlue border-perfBlue hover:text-white bg-transparent hover:bg-perfBlue  text-xs sm:text-sm p-1 md:px-2 rounded-xl ">
-            <Link to="/certificate">Certificate</Link>
-          </button>
+          <Button
+            className="border text-perfBlue border-perfBlue hover:text-white bg-transparent hover:bg-perfBlue text-xs sm:text-sm p-1 md:px-2 rounded-xl "
+            component={Link}
+            to="/certificate"
+          >
+            Certificate
+          </Button>
         </div>
         <TeamFilter />
       </div>
 
-      <div className={checked !== "Attendance" ? "hidden" : "block"}>
-        <AttendanceTable />
-      </div>
-      <div className={checked !== "Performance" ? "hidden" : "block"}>
-        <PerformanceTable />
-      </div>
-      <div className={checked !== "Team info" ? "hidden" : "block"}>
-        <TeamInfo />
-      </div>
+      {renderTable()}
     </div>
   );
 };
 
 export default CoachHome;
+
+const SkelatonComponent = () => {
+  return (
+    <>
+      <Skeleton height={100} width="95%" className="mx-auto mb-2" radius="lg" />
+      <Skeleton height={"70vh"} width="95%" className="mx-auto" radius="lg" />
+    </>
+  );
+};
