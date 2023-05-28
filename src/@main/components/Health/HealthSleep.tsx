4b7e1750bebaf300d2c4749447a6628205b958f5
mo7medhasan <87263653+mo7medhasan@users.noreply.github.com>
2023-05-29 01:03:36 +0300

    add sleep api

--- src/@main/components/Health/HealthSleep.tsx
+++ src/@main/components/Health/HealthSleep.tsx
@@ -1,49 +1,147 @@
 // @flow
 import * as React from "react";
-import { Image, RingProgress } from "@mantine/core";
-import { ChevronDownIcon } from "@heroicons/react/24/outline";
+import { Image, RingProgress, ActionIcon } from "@mantine/core";
 import { Navigation, A11y } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
-
+import moment from "moment"
+import { DatePicker } from "@mantine/dates";
+import { useFitDataMutation } from "~/app/store/health/healthApi";
+import { ChevronLeftIcon,ChevronRightIcon } from "@heroicons/react/24/solid";
 type Props = {};
 const HealthSleep = (props: Props) => {
-  const progress = 90;
-  const data = [
-    { value: 45, color: "#E19809" },
-    { value: 30, color: "#2563EB" },
-  ];
-  let hours = [];
 
-  for (let i = 1; i <= 12; i++) {
-    hours.push(i + " AM");
-  }
+  const [dateValue, setDateValue] = React.useState<Date>(new Date());
+  const [data, setData] = React.useState<[{ value: number, color: "#E19809" }, { value: number, color: "#2563EB" }]>([{ value: 0, color: "#E19809" }, { value: 0, color: "#2563EB" }]);
 
-  for (let i = 1; i <= 12; i++) {
-    hours.push(i + " PM");
-  }
+  
+
+  const [
+    FitDataActivity,
+    { data: DataActivity, isSuccess: isSuccessActivity, isLoading: isLoadingActivity, isError: isErrorActivity, error: errorActivity },
+  ] = useFitDataMutation();
+  const [
+    FitDataSleep,
+    { data: DataSleep, isSuccess: isSuccessSleep, isLoading: isLoadingSleep, isError: isErrorSleep, error: errorSleep },
+  ] = useFitDataMutation();
+
+
+  React.useEffect(() => {
+    FitDataSleep({
+      dataType: "com.google.sleep.segment",
+      Date: moment(dateValue).format("L"),
+
+    });
+    FitDataActivity({
+      dataType: "com.google.activity.segment",
+      Date: moment(dateValue).format("L"),
+
+    });
+
+  }, [dateValue]);
+
+  React.useEffect(() => {
+    if (isSuccessActivity) {
+      setData((prev) => {
+        prev[0] = { ...prev[0], value: (((calculateActivity() || 0) / 24) * 100) };
+        return [...prev];
+      })
+    }
+  }, [DataActivity, isSuccessActivity, isLoadingActivity, isErrorActivity, errorActivity]);
+  React.useEffect(() => {
+    if (isSuccessSleep) {
+      setData((prev) => {
+        prev[1] = { ...prev[1], value: (((calculateSleep() || 0) / 24) * 100) };
+        return [...prev];
+      })
+    }
+  }, [DataSleep, isSuccessSleep, isLoadingSleep, isErrorSleep, errorSleep]);
+
+  const calculateActivity = React.useCallback(() => {
+
+    const startTime = [
+      ...DataActivity?.map(
+        (bucket: { dataset: [point: [{ startTimeNanos: string }]] }) =>
+          bucket?.dataset?.map((pointItem: any) =>
+            pointItem?.point?.map(
+              (pointValue: any) => pointValue.startTimeNanos / (1000000000 * 60 * 60)
+
+            )
+          )
+      ),
+    ]
+    const endTime = [
+      ...DataActivity?.map(
+        (bucket: { dataset: [point: [{ endTimeNanos: string }]] }) =>
+          bucket?.dataset?.map((pointItem: any) =>
+            pointItem?.point?.map((pointValue: any) => pointValue.endTimeNanos / (1000000000 * 60 * 60))
+          )
+      ),
+    ]
+
+    const Time = (endTime.map((num, idx) => (parseInt(num)) - (parseInt(startTime[idx]))))
+      ?.reduce((a: number, b: number): number => Number(a) + Number(b), 0)
+      ;
+    return Time
+  }, [DataActivity, dateValue, isSuccessActivity]);
+
+  const calculateSleep = React.useCallback(() => {
+
+    const startTime = [
+      ...DataSleep?.map(
+        (bucket: { dataset: [point: [{ startTimeNanos: string }]] }) =>
+          bucket?.dataset?.map((pointItem: any) =>
+            pointItem?.point?.map(
+              (pointValue: any) => pointValue.startTimeNanos / (1000000000 * 60 * 60)
+
+            )
+          )
+      ),
+    ]
+    const endTime = [
+      ...DataSleep?.map(
+        (bucket: { dataset: [point: [{ endTimeNanos: string }]] }) =>
+          bucket?.dataset?.map((pointItem: any) =>
+            pointItem?.point?.map((pointValue: any) => pointValue.endTimeNanos / (1000000000 * 60 * 60))
+          )
+      ),
+    ]
+
+    const Time = (endTime.map((num, idx) => (parseInt(num)) - (parseInt(startTime[idx]))))
+      ?.reduce((a: number, b: number): number => Number(a) + Number(b), 0)
+
+
+    return Time
+  }, [DataSleep, dateValue, isSuccessSleep]);
+
+
 
   return (
     <div className="HealthSleep bg-white rounded-3xl w-full grid gap-9 p-4 h-full">
       <div className="flex justify-between items-center ">
         <h2 className="text-lg font-normal text-perfGray1 pb-4">Sleep</h2>
-        <span className="text-sm text-perfGray3">12 mar </span>
+        <span className="text-sm  text-perfGray3">{moment(dateValue).format('DD MMM')} </span>
       </div>
 
       <Swiper
-        navigation={true}
+
         modules={[Navigation]}
-        className="mySwiper w-full"
+        loop={true}
+        dir="rtl" className="mySwiper w-full "
+        navigation={{
+          nextEl: '.slider__next',
+          prevEl: '.slider__prev',
+        }}
       >
-        {[1, 2, 3, 4, 5, 6, 7].map((item) => (
-          <SwiperSlide>
+        {[6, 5, 4, 3, 2, 1, 0].map((item, index) => (
+          <SwiperSlide key={item} dir="ltr">
             <div className=" w-full flex justify-center gap-2 px-3">
               <div className=" gap-4 flex flex-col">
                 <div className="flex-1 gap-2 flex flex-col">
                   <div className=" gap-2 flex justify-start items-center ">
                     <div className="w-[15px] h-[15px] rounded-full bg-perfBlue" />{" "}
-                    {data[0].value} hr
+                    {((data[1].value / 100) * 24).toFixed(0)} hr
                   </div>
                   sleep
                 </div>
@@ -51,7 +149,7 @@ const HealthSleep = (props: Props) => {
                   <div className="flex-1 gap-2 flex flex-col">
                     <div className=" gap-2 flex justify-start items-center ">
                       <div className="w-[15px] h-[15px] rounded-full bg-[#E19809]" />{" "}
-                      {data[1].value} hr
+                      {((data[0].value / 100) * 24).toFixed(0)} hr
                     </div>
                     activities
                   </div>
@@ -65,52 +163,68 @@ const HealthSleep = (props: Props) => {
                   classNames={{
                     label:
                       "shadow bg-white h-[55%] w-[55%] text-center grid justify-center items-center rounded-full  m-auto z-0 left-auto right-auto ",
-                    root: ` last:[&_circle]:!stroke-[45px] [&:nth-child(2)]:[&_circle]:!stroke-[36px] last:[&_circle]:drop-shadow-xl [&:nth-child(2)]:[&_circle]:!stroke-[36px] [&:nth-child(2)]:[&_circle]:!drop-shadow-xl drop-shadow-lg 
+                    root: `[&_circle]:duration-1000	 [&_circle]:transition-all   duration-1000  transition-all last:[&_circle]:!stroke-[45px] [&:nth-child(2)]:[&_circle]:!stroke-[36px] last:[&_circle]:drop-shadow-xl [&:nth-child(2)]:[&_circle]:!stroke-[36px] [&:nth-child(2)]:[&_circle]:!drop-shadow-xl drop-shadow-lg 
                     first:[&_circle]:drop-shadow	first:[&_circle]:stroke-[#BDBDBD] relative flex justify-center items-center`,
                   }}
                   label={
-                    <div className=" grid justify-center items-center  l  gap-2 ">
-                      <Image
-                        width={29}
-                        height={29}
-                        src={"/assets/images/Groupface.png"}
-                        alt={"footprint"}
-                        className="mx-auto"
-                      />
-                      <div className="text-lg text-perfGray1"> good</div>
+                    <div className="grid justify-center items-center gap-2">
+                    <Image
+                      width={29}
+                      height={29}
+                      src={
+                        ((data[0].value / 100) * 24 < 8 || (data[1].value / 100) * 24 > 8) ? "/assets/images/GroupBadface.png" :
+                        ((data[0].value / 100) * 24 < 8 && (data[0].value / 100) * 24 > 6 &&
+                          (data[1].value / 100) * 24 < 12) && (data[1].value / 100) * 24 > 8 ? "/assets/images/Groupface.png" :
+                          ((data[0].value / 100) * 24 < 8 && (data[0].value / 100) * 24 > 6 &&
+                            (data[1].value / 100) * 24 > 12) ? "/assets/images/GroupExcellentFace.png" : "/assets/images/Groupface.png"
+                        
+                        }
+                      alt={"footprint"}
+                      className="mx-auto"
+                    />
+                    <div className={`text-lg ${((data[0].value / 100) * 24 < 8 || (data[1].value / 100) * 24 > 8) ? "text-red" :
+                      ((data[0].value / 100) * 24 < 8 && (data[0].value / 100) * 24 > 6 &&
+                        (data[1].value / 100) * 24 < 12) && (data[1].value / 100) * 24 > 8 ? "text-green" : "text-perfGray1"}`}>
+                      {((data[0].value / 100) * 24 < 8 || (data[1].value / 100) * 24 > 8) ? "bad" :
+                        ((data[0].value / 100) * 24 < 8 && (data[0].value / 100) * 24 > 6 &&
+                          (data[1].value / 100) * 24 < 12) && (data[1].value / 100) * 24 > 8 ? "good" :
+                          ((data[0].value / 100) * 24 < 8 && (data[0].value / 100) * 24 > 6 &&
+                            (data[1].value / 100) * 24 > 12) ? "Excellent" : "good"}
                     </div>
+                  </div>
+                  
                   }
                 />
               </div>
             </div>
           </SwiperSlide>
         ))}
+        <div className="flex justify-between items-center absolute inset-0 z-30 slider pb-16">    
+        <ActionIcon onClick={()=>{
+            if(moment(dateValue).format("L")!==moment().format("L")) { setDateValue((prev) => {
+                const newDate = new Date(prev);
+                newDate.setDate(newDate.getDate() + 1);
+                return newDate;
+              });}
+          }}
+         disabled={moment(dateValue).format("L")==moment().format("L")? true:false} className="slider__prev border-[.5px] border-[#99999995] rounded-full w-10 h-10  shadow-lg  text-xl !bg-white text-blue hover:!bg-white/90">
+            <ChevronRightIcon
+              className="w-6 h-6 "
+            />
+          </ActionIcon> <ActionIcon onClick={()=>{
+              setDateValue((prev) => {
+                const newDate = new Date(prev);
+                newDate.setDate(newDate.getDate() - 1);
+                return newDate;
+              });
+          }} className="slider__next  border-[.5px] border-[#99999995] rounded-full w-10 h-10  shadow-lg  text-xl !bg-white text-blue hover:!bg-white/90">
+            <ChevronLeftIcon
+              className="w-6 h-6 "
+            />
+          </ActionIcon></div>
       </Swiper>
-      <Swiper
-        slidesPerView={"auto"}
-        spaceBetween={10}
-        modules={[A11y]}
-        className="mySwiper w-full"
-      >
-        {hours.map((item) => (
-          <SwiperSlide
-            className={`w-10 h-[110px] p-2  grid content-between justify-center  `}
 
-          >
-            <div
-              className={`w-10 h-[70px] p-2 rounded-[10px] text-white flex content-between justify-center
-            text-center items-center  `}
-              style={{ backgroundColor: `${parseInt(item) <= 7 ? "#2F80ED" : "#BDBDBD"}` }}
-            >
-              {item}</div>
-            <div className="w-4 h-4 rounded-full mx-auto"
-              style={{
-                backgroundColor: `${parseInt(item) <= 7 ? "#2F80ED" : "#BDBDBD"}`,
-                display: [5, 2, 8].includes(parseInt(item)) ? "flex" : "none"
-              }} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+
     </div>
   );
 };
