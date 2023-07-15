cf0dc6df1d79be983de704172597cb0a036f6289
mo7medhasan <87263653+mo7medhasan@users.noreply.github.com>
2023-07-15 16:30:53 +0300

    update TOKEN

--- src/app/store/chatAi/chatAiApi.ts
+++ src/app/store/chatAi/chatAiApi.ts
@@ -1,14 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { BASE_URL2 } from "~/app/configs/dataService";
-const TOKEN = import.meta.env.VITE_TOKEN;
+import { BASE_HEADERS, BASE_URL2 } from "~/app/configs/dataService";
+
 export const chatAiApi = createApi({
   reducerPath: "chatAiApi",
   baseQuery: fetchBaseQuery({
     baseUrl: `${BASE_URL2}/v1/`,
-    prepareHeaders: (headers: any, api: any) => {
-      headers.set("Authorization", `Bearer ${TOKEN}`);
-       return headers;
-    }
+    prepareHeaders: BASE_HEADERS,
     
   }),
   tagTypes: ["chatAi"],
